2c4f1f5cf053e92f873a3529654c518707a13da9
etnothomas <thomas.pol.sci@gmail.com>
2026-02-22 23:23:00 +0100

    adding -gitignore

--- obj/Debug/net10.0/VontobelTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/VontobelTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VontobelTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e9c0db4a81e5cef6cba6377e5a23363093ef15")]
 [assembly: System.Reflection.AssemblyProductAttribute("VontobelTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VontobelTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
